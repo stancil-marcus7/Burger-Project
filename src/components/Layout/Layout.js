e46a7a661ce22214abd86bb6bc63ab8aeac8ef8e
Marcus Stancil <stancil.marcus@yahoo.com>
2019-07-24 15:47:31 -0400

    Add Layout, BurgerBuilder and Aux components

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css';
+
+//Layout will house the Tookbar, SideDrawer, and Backdrop components
+const layout = (props) => (
+    <Aux>
+        <div>
+            Toolbar SideDrawer Backdrop
+        </div>
+        
+        {/* The CSS for main is used to put distance between the BurgerBuilder component and the components in Layout */}
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
+)
+
+export default layout;
---
+++ src/hoc/Aux.js
@@ -0,0 +1,4 @@
+//We'll use this as higher order component so that we can have adjacent components in the Layout component
+const aux = (props) => props.children;
+
+export default aux;
