2ae215ac34f57764505b3622566276ad65011b23
Marcus Stancil <stancil.marcus@yahoo.com>
2019-07-25 10:07:58 -0400

    Made arrays single array for BurgerIngredients in Burger component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,14 +5,23 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 //This component will serve as a wrapper for the BurgerIngredient components
 const burger = (props) => {
     //transformedIngredients gets the ingredients from the state of the BurgerBuilder component and transforms them into BurgerIngredients
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_,i)=>{
                 return <BurgerIngredient key={igKey + i} type={igKey}/>
             });
-        });
+            //This reduces everything to a single array for the BurgerIngredients
+        }).reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
     
-
+    //If no ingredients are present, this message will display between the two burger buns
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please startt adding ingredients!</p>
+    }
+    
+    console.log(transformedIngredients);
+    
     return(
         <div className={classes.Burger}>
             {/* Rendering the ingredients */}
