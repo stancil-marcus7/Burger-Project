4ec31231aa27e99adc1fcf1fcb1e4dfb09eb2f7f
Marcus Stancil <stancil.marcus@yahoo.com>
2019-07-29 11:23:33 -0400

    Add backdrop disappearance functionality

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import Toolbar from '../Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 //Layout will house the Tookbar, SideDrawer, and Backdrop components
-const layout = (props) => (
-    <Aux>
-        <div>
-            <Toolbar/>
-            <SideDrawer/>
-        </div>
-        {/* The CSS for main is used to put distance between the BurgerBuilder component and the components in Layout */}
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+    
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: closed})
+    }
 
-export default layout;
+    render(){
+        return(
+            <Aux>
+                <div>
+                    <Toolbar/>
+                    <SideDrawer
+                        open={this.state.showSideDrawer} 
+                        closed={this.sideDrawerClosedHandler}/>
+                </div>
+                {/* The CSS for main is used to put distance between the BurgerBuilder component and the components in Layout */}
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+            )
+}
+}
+    
+
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,15 +2,23 @@ import React from 'react';
 import classes from "./SideDrawer.module.css";
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NativationItems'
+import Backdrop from "../../UI/Backdrop/Backdrop"
+import Aux from '../../../hoc/Aux'
 
 const sideDrawer = (props) => {
     return (
-        <div className={classes.SideDrawer}>
-            <Logo height="11%"/>
-                <nav>
-                    <NavigationItems/>
-                </nav>
-        </div>
+        <Aux>
+            {/* Once the backdrop is clicked, it will disappear */}
+            <Backdrop show={props.open} clicked={props.closed}/>
+                <div className={classes.SideDrawer}>
+                    <div className={classes.Logo}>
+                        <Logo/>
+                    </div>
+                        <nav>
+                            <NavigationItems/>
+                        </nav>
+                </div>
+        </Aux>
     );
 };
 
