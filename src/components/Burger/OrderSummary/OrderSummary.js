8fc4dedb89ba27c0ffa74cf97fe0b2d8806bc053
Marcus Stancil <stancil.marcus@yahoo.com>
2019-07-29 15:39:15 -0400

    Stop OrderSummary from rendering unncecessarily

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,38 @@
-import React from 'react'
+import React, {Component} from 'react'
+
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button'
 
 //This component will house the summary of the order
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    componentDidUpdate(){
+        console.log("[OrderSummary] ComponentDidUpdate")
+    }
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return(<li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
                 </li>)
         })
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Check Out?</p>
+                <Button btnType={"Danger"} click={this.props.cancelPurchase}>CANCEL</Button>
+                <Button btnType={"Success"} click={this.props.purchase}>CONTINUE</Button>
+            </Aux>
+    );
     
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to Check Out?</p>
-            <Button btnType={"Danger"} click={props.cancelPurchase}>CANCEL</Button>
-            <Button btnType={"Success"} click={props.purchase}>CONTINUE</Button>
-        </Aux>
-);
+    }
 }
+    
+    
+    
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,34 @@
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Aux';
-import React from 'react';
+import React, {Component} from 'react';
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = (props) => ( 
-    <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed}/>
-    <div className={classes.Modal} style={{
-        //If props.show is true the modal will show, if now it will slide out the way
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        //If props.shpw is true the opacity will be 1; if not it will be zero
-        opacity: props.show ? '1':'0'
-    }}>
-        {props.children}
-    </div>
-    </Aux>
-)
+class Modal extends Component{
+    
+    //Will restrict modal from rendering until the show property is different from the previous state's
+    shouldComponentUpdate ( nextProps, nextState ){
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    componentDidUpdate () {
+        console.log('[Modal] WillUpdate')
+    }
+    
+    render(){
+        return(
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={classes.Modal} style={{
+                    //If props.show is true the modal will show, if now it will slide out the way
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    //If props.shpw is true the opacity will be 1; if not it will be zero
+                    opacity: this.props.show ? '1':'0'
+                }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
+
+export default Modal;
